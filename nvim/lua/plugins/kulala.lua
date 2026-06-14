e9806839d3665e433779075a0a5e7ae7853d81ff
hishi <shunsuke.hishikawa@gmail.com>
2026-06-14 11:33:03 +0900

    feat: kulalaにレスポンス履歴ナビゲーション用のキーマップを追加

--- nvim/lua/plugins/kulala.lua
+++ nvim/lua/plugins/kulala.lua
@@ -9,6 +9,20 @@ return {
   opts = {
     global_keymaps = true,
     global_keymaps_prefix = "<leader>R",
-    kulala_keymaps_prefix = "",
+    kulala_keymaps_prefix = "<leader>k",
+    kulala_keymaps = {
+      ["Previous response"] = {
+        "H",
+        function()
+          require("kulala.ui").show_previous()
+        end,
+      },
+      ["Next response"] = {
+        "L",
+        function()
+          require("kulala.ui").show_next()
+        end,
+      },
+    },
   },
 }
