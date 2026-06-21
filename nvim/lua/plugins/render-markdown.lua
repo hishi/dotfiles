f358ed889526e508d6c755fdf43457ec6599795f
hishi <shunsuke.hishikawa@gmail.com>
2026-06-21 14:27:20 +0900

    feat: render-markdownのプレビュー機能とテーブル・見出しのレンダリング設定を改善

--- nvim/lua/plugins/render-markdown.lua
+++ nvim/lua/plugins/render-markdown.lua
@@ -4,7 +4,16 @@ return {
     cmd = { "RenderMarkdown" },
     ft = { "markdown", "markdown.mdx", "Avante", "codecompanion" },
     keys = {
-      { "<leader>op", "<cmd>RenderMarkdown preview<cr>", ft = "markdown", desc = "Markdown Preview (Split)" },
+      {
+        "<leader>op",
+        function()
+          local rm = require("render-markdown")
+          rm.enable()
+          rm.preview()
+        end,
+        ft = "markdown",
+        desc = "Markdown Preview (Split)",
+      },
     },
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
@@ -13,6 +22,23 @@ return {
     opts = {
       enabled = false,
       file_types = { "markdown", "Avante", "codecompanion" },
+      pipe_table = {
+        preset = "round",
+        style = "full",
+        cell = "padded",
+        min_width = 8,
+        border_virtual = true,
+      },
+      heading = {
+        -- Keep H1/H2 readable without the visual indent/padded full-line background.
+        sign = false,
+        position = "inline",
+        width = "block",
+        left_margin = 0,
+        left_pad = 0,
+        right_pad = 0,
+        icons = { "H1 ", "H2 ", "H3 ", "H4 ", "H5 ", "H6 " },
+      },
     },
   },
 }
